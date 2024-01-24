2828b94b150a713d9ca80d210ec18defad82820d
Lutz Dietterich <lutzdietterich@googlemail.com>
2024-01-24 15:48:52 +0100

    init tailwind.css and created FormTransaction component

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,14 +1,17 @@
 import Image from "next/image";
+import FormTansaction from "@/components/FormTansaction";
 
 export default function Home() {
   return (
-    <main>
+    <main className=" w-full">
       <Image
+        className="m-auto"
         src="/logo192x192.png"
         width={192}
         height={192}
         alt="Logo Kassen kaptain"
       />
+      <FormTansaction />
     </main>
   );
 }
---
+++ src/components/FormTansaction.jsx
@@ -0,0 +1,18 @@
+"use client";
+
+import React from "react";
+import styled from "styled-components";
+
+export default function FormTansaction() {
+  return (
+    <>
+      <StyledWrapper>FormTansaction</StyledWrapper>
+    </>
+  );
+}
+
+const StyledWrapper = styled.div`
+  min-width: 90%;
+  min-height: 400px;
+  background-color: white;
+`;
